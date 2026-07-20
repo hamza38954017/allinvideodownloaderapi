035014a9939e92b07e5315de0ca011e2222ea39a
hamza38954017 <hamza38954017@gmail.com>
2026-07-21 03:40:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -98,8 +98,7 @@ def extract_info_sync(url: str, proxy: str = None):
     ydl_opts = {
         'quiet': True,
         'no_warnings': True,
-        'skip_download': True,
-        'force_ipv6': True
+        'skip_download': True
     }
     if proxy:
         ydl_opts['proxy'] = proxy
